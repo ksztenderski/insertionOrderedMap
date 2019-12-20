dd9fd7b635c7779972bb523ddf0dead06f22c773
Krystian Sztenderski <sztenderskikrystian@gmail.com>
2019-12-21 00:31:30 +0100

    poprawiona obsługa wyjątków

--- insertion_ordered_map.h
+++ insertion_ordered_map.h
@@ -9,273 +9,283 @@
 
 
 class lookup_error : std::exception {
-	[[nodiscard]] const char *what() const noexcept override {
-		return "lookup_error";
-	}
+    [[nodiscard]] const char *what() const noexcept override {
+        return "lookup_error";
+    }
 };
 
 template<class K, class V, class Hash = std::hash<K>>
 class insertion_ordered_map {
 private:
-	using list_t = std::list<std::pair<K, V>>;
-	using map_t = std::unordered_map<K, typename std::list<std::pair<K, V>>::iterator, Hash>;
+    using list_t = std::list<std::pair<K, V>>;
+    using map_t = std::unordered_map<K, typename std::list<std::pair<K, V>>::iterator, Hash>;
 
-	std::shared_ptr<list_t> list;
-	std::shared_ptr<map_t> map;
+    std::shared_ptr<list_t> list;
+    std::shared_ptr<map_t> map;
 
-	bool must_be_copied;
+    bool mustBeCopied;
 
-	void copyList() {
-		list_t newList(*list);
-		list.reset();
-		list = std::make_shared<list_t>(newList);
-	}
+    void copyList() {
+        list_t newList(*list);
+        list.reset();
+        list = std::make_shared<list_t>(newList);
+    }
 
-	void copyMap() {
-		map_t newMap;
+    void copyMap() {
+        map_t newMap;
 
-		for (auto itr = list->begin(); itr != list->end(); ++itr) {
-			newMap.insert({itr->first, itr});
-		}
+        for (auto itr = list->begin(); itr != list->end(); ++itr) {
+            newMap.insert({itr->first, itr});
+        }
 
-		map.reset();
-		map = std::make_shared<map_t>(newMap);
-	}
+        map.reset();
+        map = std::make_shared<map_t>(newMap);
+    }
 
 public:
-	insertion_ordered_map() {
-		list = std::make_shared<list_t>();
-		map = std::make_shared<map_t>();
-		must_be_copied = false;
-	}
+    insertion_ordered_map() {
+        list = std::make_shared<list_t>();
+        map = std::make_shared<map_t>();
+        mustBeCopied = false;
+    }
 
     insertion_ordered_map(insertion_ordered_map const &other) {
-        if (other.must_be_copied) {
+        if (other.mustBeCopied) {
             list = std::make_shared<list_t>(list_t(*other.list));
             copyMap();
         } else {
             list = other.list;
             map = other.map;
         }
-        must_be_copied = false;
+        mustBeCopied = false;
     }
 
-	insertion_ordered_map(insertion_ordered_map &&other) noexcept {
-		list = std::move(other.list);
-		map = std::move(other.map);
-		must_be_copied = false;
-	}
+    insertion_ordered_map(insertion_ordered_map &&other) noexcept {
+        list = std::move(other.list);
+        map = std::move(other.map);
+        mustBeCopied = false;
+    }
 
-	insertion_ordered_map &operator=(insertion_ordered_map other) {
-		map = other.map;
-		list = other.list;
-		return *this;
-	}
+    insertion_ordered_map &operator=(insertion_ordered_map other) {
+        map = other.map;
+        list = other.list;
+        return *this;
+    }
 
-	bool insert(K const &k, V const &v) {
-		list_t* aux_list = list.get();
-		map_t* aux_map = map.get();
-		try {
-			if (!list.unique()) copyList();
-			if (!map.unique()) copyMap();
-			must_be_copied = false;
-		} catch (std::exception& e) {
-			list.reset(aux_list);
-			map.reset(aux_map);
-			throw e;
-		}
+    bool insert(K const &k, V const &v) {
+        list_t *aux_list = list.get();
+        map_t *aux_map = map.get();
+        try {
+            if (!list.unique()) copyList();
+            if (!map.unique()) copyMap();
+        } catch (std::exception &e) {
+            list.reset(aux_list);
+            map.reset(aux_map);
+            throw e;
+        }
 
-		auto found = map->find(k);
-		if (found == map->end()) {
-			std::pair<K, V> list_pair = std::make_pair(k, v);
-			list->push_back(list_pair);
-			std::pair<K, typename std::list<std::pair<K, V>>::iterator>
-					map_pair = make_pair(k, --list->end());
-			try {
-				map->insert(map_pair);
-			} catch(std::exception& e) {
-				list->pop_back();
-				throw e;
-			}
+        mustBeCopied = false;
 
-			return true;
-		} else {
-			list->splice(list->end(), *list, found->second);
+        auto found = map->find(k);
+        if (found == map->end()) {
+            std::pair<K, V> list_pair = std::make_pair(k, v);
+            list->push_back(list_pair);
+            try {
+                std::pair<K, typename std::list<std::pair<K, V>>::iterator>
+                        map_pair = make_pair(k, --list->end());
+                map->insert(map_pair);
+            } catch (std::exception &e) {
+                list->pop_back();
+                throw e;
+            }
 
-			return false;
-		}
-	}
+            return true;
+        } else {
+            list->splice(list->end(), *list, found->second);
 
-	void erase(K const &k) {
-		list_t* aux_list = list.get();
-		map_t* aux_map = map.get();
-		try {
-			if (!list.unique()) copyList();
-			if (!map.unique()) copyMap();
-			must_be_copied = false;
-		} catch(std::exception& e) {
-			list.reset(aux_list);
-			map.reset(aux_map);
-			throw e;
-		}
+            return false;
+        }
+    }
 
-		auto tmp = map->find(k);
-		if (tmp == map->end()) {
-			throw lookup_error();
-		}
+    void erase(K const &k) {
+        list_t *aux_list = list.get();
+        map_t *aux_map = map.get();
+        try {
+            if (!list.unique()) copyList();
+            if (!map.unique()) copyMap();
+            mustBeCopied = false;
+        } catch (std::exception &e) {
+            list.reset(aux_list);
+            map.reset(aux_map);
+            throw e;
+        }
 
-		list->erase(tmp->second);
-		map->erase(k);
-	}
+        auto tmp = map->find(k);
+        if (tmp == map->end()) {
+            throw lookup_error();
+        }
 
-	void merge(insertion_ordered_map const &other) {
-		list_t* aux_list = list.get();
-		map_t* aux_map = map.get();
-		try {
-			if (!list.unique()) copyList();
-			if (!map.unique()) copyMap();
-			must_be_copied = false;
-		} catch(std::exception& e) {
-			list.reset(aux_list);
-			map.reset(aux_map);
-			throw e;
-		}
+        map->erase(k);
+        list->erase(tmp->second);
+    }
 
-		for (auto item : *other.list) {
-			insert(item.first, item.second);
-		}
-	}
+    void merge(insertion_ordered_map const &other) {
+        list_t *aux_list = list.get();
+        map_t *aux_map = map.get();
+        try {
+            copyList();
+            copyMap();
 
-	V &at(K const &k) {
-		list_t* aux_list = list.get();
-		map_t* aux_map = map.get();
-		try {
-			if (!list.unique()) copyList();
-			if (!map.unique()) copyMap();
-		} catch(std::exception& e) {
-			list.reset(aux_list);
-			map.reset(aux_map);
-			throw e;
-		}
-		must_be_copied = true;
+            for (auto item : *other.list) {
+                insert(item.first, item.second);
+            }
 
-		try {
-			return map->at(k)->second;
-		}
-		catch (const std::out_of_range& e) {
-			throw lookup_error();
-		}
-	}
+            mustBeCopied = false;
+        } catch (std::exception &e) {
+            list.reset(aux_list);
+            map.reset(aux_map);
+            throw e;
+        }
+    }
 
-	V const &at(K const &k) const {
-		try {
-			return map->at(k)->second;
-		}
-		catch (const std::out_of_range& e) {
-			throw lookup_error();
-		}
-	}
+    V &at(K const &k) {
+        list_t *aux_list = list.get();
+        map_t *aux_map = map.get();
+        bool mustBeCopiedBefore = mustBeCopied;
 
-	V &operator[](K const &k) {
-		list_t* aux_list = list.get();
-		map_t* aux_map = map.get();
-		try {
-			if (!list.unique()) copyList();
-			if (!map.unique()) copyMap();
-		} catch(std::exception& e) {
-			list.reset(aux_list);
-			map.reset(aux_map);
-			throw e;
-		}
-		must_be_copied = true;
+        try {
+            if (!list.unique()) copyList();
+            if (!map.unique()) copyMap();
+        } catch (std::exception &e) {
+            list.reset(aux_list);
+            map.reset(aux_map);
+            throw e;
+        }
+        mustBeCopied = true;
 
-		if (map->find(k) == map->end()) {
-			insert(k, V());
-		}
+        try {
+            return map->at(k)->second;
+        }
+        catch (const std::out_of_range &e) {
+            mustBeCopied = mustBeCopiedBefore;
+            throw lookup_error();
+        }
+    }
 
-		return map->at(k)->second;
-	}
+    V const &at(K const &k) const {
+        try {
+            return map->at(k)->second;
+        }
+        catch (const std::out_of_range &e) {
+            throw lookup_error();
+        }
+    }
 
-	[[nodiscard]] size_t size() const {
-		return list->size();
-	}
+    V &operator[](K const &k) {
+        list_t *aux_list = list.get();
+        map_t *aux_map = map.get();
+        bool mustBeCopiedBefore = mustBeCopied;
 
-	[[nodiscard]] bool empty() const {
-		return list->empty();
-	}
+        try {
+            if (!list.unique()) copyList();
+            if (!map.unique()) copyMap();
 
-	void clear() {
-		list_t* aux_list = list.get();
-		map_t* aux_map = map.get();
-		try {
-			if (!list.unique()) {
-				list.reset();
-				list = std::make_shared<list_t>();
-			} else {
-				list->clear();
-			}
+            if (map->find(k) == map->end()) {
+                insert(k, V{});
+            }
 
-			if (!map.unique()) {
-				map.reset();
-				map = std::make_shared<map_t>();
-			} else {
-				map->clear();
-			}
-			must_be_copied = false;
-		} catch(std::exception& e) {
-			list.reset(aux_list);
-			map.reset(aux_map);
-			throw e;
-		}
-	}
+            mustBeCopied = true;
 
-	bool contains(K const &k) const {
-		return map->find(k) != map->end();
-	}
+            return map->at(k)->second;
+        } catch (std::exception &e) {
+            list.reset(aux_list);
+            map.reset(aux_map);
+            mustBeCopied = mustBeCopiedBefore;
+            throw e;
+        }
+    }
 
-	class iterator {
-		typename list_t::const_iterator itr;
+    [[nodiscard]] size_t size() const noexcept {
+        return list->size();
+    }
 
-	public:
-		friend insertion_ordered_map;
+    [[nodiscard]] bool empty() const noexcept {
+        return list->empty();
+    }
 
-		iterator() = default;
+    void clear() {
+        list_t *aux_list = list.get();
+        map_t *aux_map = map.get();
 
-		iterator(iterator const &other) : itr(other.itr) {}
+        try {
+            if (!list.unique()) {
+                list.reset();
+                list = std::make_shared<list_t>();
+            } else {
+                list->clear();
+            }
 
-		iterator &operator++() {
-			++itr;
-			return *this;
-		}
+            if (!map.unique()) {
+                map.reset();
+                map = std::make_shared<map_t>();
+            } else {
+                map->clear();
+            }
+            mustBeCopied = false;
+        } catch (std::exception &e) {
+            list.reset(aux_list);
+            map.reset(aux_map);
+            throw e;
+        }
+    }
 
-		bool operator==(const iterator &rhs) const {
-			return itr == rhs.itr;
-		}
+    bool contains(K const &k) const {
+        return map->find(k) != map->end();
+    }
 
-		bool operator!=(const iterator &rhs) const {
-			return !(rhs == *this);
-		}
+    class iterator {
+        typename list_t::const_iterator itr;
 
-		const std::pair<K, V> &operator*() const {
-			return *itr;
-		}
+    public:
+        friend insertion_ordered_map;
 
-		const std::pair<K, V> *operator->() const {
-			return &(*itr);
-		}
-	};
+        iterator() = default;
 
-	iterator begin() const {
-		iterator iterator;
-		iterator.itr = list->cbegin();
-		return iterator;
-	}
+        iterator(iterator const &other) : itr(other.itr) {}
 
-	iterator end() const {
-		iterator iterator;
-		iterator.itr = list->cend();
-		return iterator;
-	}
+        iterator &operator++() {
+            ++itr;
+            return *this;
+        }
+
+        bool operator==(const iterator &rhs) const {
+            return itr == rhs.itr;
+        }
+
+        bool operator!=(const iterator &rhs) const {
+            return !(rhs == *this);
+        }
+
+        const std::pair<K, V> &operator*() const {
+            return *itr;
+        }
+
+        const std::pair<K, V> *operator->() const {
+            return &(*itr);
+        }
+    };
+
+    iterator begin() const noexcept {
+        iterator iterator;
+        iterator.itr = list->cbegin();
+        return iterator;
+    }
+
+    iterator end() const noexcept {
+        iterator iterator;
+        iterator.itr = list->cend();
+        return iterator;
+    }
 };
 
 #endif //INSERTION_ORDERED_MAP_H
